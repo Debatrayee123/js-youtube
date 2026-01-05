9bb80c756ffa78afcfd84352b857ad5457259424
Debatrayee123 <debatrayee2030@gmail.com>
2026-01-05 15:10:10 +0000

    variables done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId =2 
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+accountEmail="hc@hc.com"
+accountPassword ="21212121"
+accountCity ="Bengaluru"
